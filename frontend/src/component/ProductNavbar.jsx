6c7cca00120108b38f6a253ddae462158976a79c
prem sharma <mybrother1370@gmail.com>
2025-04-16 20:29:31 +0530

    Update ProductNavbar.jsx

--- frontend/src/component/ProductNavbar.jsx
+++ frontend/src/component/ProductNavbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 import { UserContext } from '../config/userContext'
+import { axiosInstance } from '../config/axios';
 
 const ProductNavbar = () => {
     const navigate = useNavigate();
